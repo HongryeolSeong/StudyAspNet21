83ab367da0c69da7e8123ac8b165e574b52e4090
ghd0276 <ghd0276@naver.com>
2021-04-15 17:33:55 +0900

    Revert "4/15 게시판 글쓰기 완료"
    This reverts commit ed29930dd3ea2bcde998a1e948b696d3128f92e8.

--- DotNetNote/DotNetNote/Board/BoardList.aspx.cs
+++ DotNetNote/DotNetNote/Board/BoardList.aspx.cs
@@ -17,6 +17,8 @@ namespace DotNetNote.Board
 
         public int RecordCount = 0; // 총 레코드 수
 
+        public int PageIndex = 0; // 페이징시 필수 값, 현재 보여줄 페이지 번호
+
         public BoardList()
         {
             _repo = new DbRepository(); // SqlConnection 생성
@@ -27,8 +29,23 @@ namespace DotNetNote.Board
             if (!SearchMode)
             {
                 RecordCount = _repo.GetCountAll();
-                LblTotalRecord.Text = $"Total Record : {RecordCount}";
             }
+            LblTotalRecord.Text = $"Total Record : {RecordCount}";
+
+            if(Request["Page"] != null)
+            {
+                PageIndex = Convert.ToInt32(Request["Page"]) - 1;
+            }
+            else
+            {
+                PageIndex = 0; // 1페이지
+            }
+
+            // TODO : 쿠키 사용해서 리스트 페이지번호 유지
+
+            // 페이징 처리
+            PagingControl.PageIndex = PageIndex;
+            PagingControl.RecordCount = RecordCount;
 
             if (!Page.IsPostBack)
             {
@@ -40,7 +57,7 @@ namespace DotNetNote.Board
         {
             if (!SearchMode)
             {
-                GrvNotes.DataSource = _repo.GetAll(0); // 페이징 시작 : 0
+                GrvNotes.DataSource = _repo.GetAll(PageIndex); // 페이징 시작 : 0
             }
 
             GrvNotes.DataBind(); // 데이터바인딩
